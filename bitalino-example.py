0a54f9e494f0f445775eb4be2628aeeba13ff47e
Mark IJzerman <markijzerman@gmail.com>
2018-07-03 11:18:43 +0200

    mac address is just the /dev/tty address. add gitignore

---
+++ bitalino-example.py
@@ -0,0 +1,40 @@
+import bitalino
+from bitalino import BITalino
+
+# This example will collect data for 5 sec.
+macAddress = "/dev/tty.BITalino-DevB" # on Mac OS replace XX-XX by the 4 final digits of the MAC address
+running_time = 5
+    
+batteryThreshold = 30
+acqChannels = [0, 1, 2, 3, 4, 5]
+samplingRate = 1000
+nSamples = 10
+digitalOutput = [1,1]
+
+# Connect to BITalino
+device = BITalino(macAddress)
+
+# Set battery threshold
+device.battery(batteryThreshold)
+
+# Read BITalino version
+print(device.version())
+    
+# Start Acquisition
+device.start(samplingRate, acqChannels)
+
+start = time.time()
+end = time.time()
+while (end - start) < running_time:
+    # Read samples
+    print(device.read(nSamples))
+    end = time.time()
+
+# Turn BITalino led on
+device.trigger(digitalOutput)
+    
+# Stop acquisition
+device.stop()
+    
+# Close connection
+device.close()
